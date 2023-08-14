f29861047b4fd5cf838a91ba7867356c7f7a15ef
aiden <aiden@localhost.localdomain>
2023-08-13 20:22:52 -0400

    Add home sections

--- space-site-v2/src/home.jsx
+++ space-site-v2/src/home.jsx
@@ -1,14 +1,18 @@
 import React, { useRef } from 'react';
+import './styling/home.scss'
+import earthImage from './assets/photos/earth.png'
+import moonImage from './assets/photos/moon.png'
+import marsImage from './assets/photos/mars.png'
 
 function home() {
     return (
         <>
-            <div className='parallax-group'>
+            <div className='parallax-group' id='home-container'>
                 <div id='home-intro-container' className='parallax_layer--base'>
                     <h1>THE HISTORY OF SPACE TRAVEL</h1>
                     <p>THE STORY BEGINS NOW</p>
                 </div>
-            </div>
+            </div>      
 
             <div id='home-sections' className='parallax_group'>
                 <div
@@ -16,6 +20,7 @@ function home() {
                     className='parallax__layer parallax__layer--back'
                 >
                     <h2>PAST</h2>
+                    <img src={earthImage} alt='Earth' className='home-image' />
                 </div>
 
                 <div
@@ -23,6 +28,15 @@ function home() {
                     className='parallax__layer parallax__layer--back'
                 >
                     <h2>PRESENT</h2>
+                    <img src={moonImage} alt="Moon" className='home-image' />
+                </div>
+
+                <div
+                    id='home-future'
+                    className='parallax__layer parallax__layer--back'
+                >
+                    <h2>FUTURE</h2>
+                    <img src={marsImage} alt="Mars" className='home-image' />
                 </div>
             </div>
         </>
