471795486909a5565ca679648f3f5b1290acda84
aiden <aidenrphotos@gmail.com>
2023-08-17 22:46:09 -0400

    Add Goddard text

--- space-site-v2/src/home.jsx
+++ space-site-v2/src/home.jsx
@@ -38,6 +38,17 @@ function home() {
                 
                 <div id='goddard-div'>
                 <p>ROBERT GODDARD</p>
+                <p>Dr. Robert H. Goddard was an American physicist, engineer, inventor,
+            and professor. Robert was born October 5, 1882 in Worcester Massachusetts,
+            and as a young boy he had severe stomach issues which resulted in him missing two
+            years of school. When he matured he became a varacious reader and made regular
+            trips to his local library. Goddard later attended Worcester Polytechnic Institue,
+            Where he earned a B.S. degree in physics in 1908. His whole life he was fascinated
+            with flight and space, and in 1915 Goddard performed his first launch of a gunpowder-fueled
+            rocket. He noticed that gunpowder was extremely insufficient at converting energy into 
+            thrust and motion. This later lead to Goddard creating the first ever liquid proppeled 
+            rocket in 1926. This rocket was launched on March 16, 1926, the flight lasted 2.5 seconds
+            and flew to a height of 41 feet at an everage speed of 60mph.</p>
             </div>
             </div>
         </>
